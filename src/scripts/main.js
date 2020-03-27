c7675d861c03988afe78e01bfc9a57aefe4a69b2
Vitalii <vitaliybondarenko1982@gmail.com>
2020-03-27 23:09:31 +0200

    implemented base layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,471 @@
+/* eslint-disable max-len */
+/* eslint-disable semi */
 'use strict';
+
+const news = [
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+
+  {
+    id: 'world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    type: 'article',
+    sectionId: 'world',
+    sectionName: 'World news',
+    webPublicationDate: '2020-01-06T12:23:12Z',
+    webTitle: 'Iranian ambassador hits out at ‘provocative’ Times article',
+    webUrl: 'https://www.theguardian.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    apiUrl: 'https://content.guardianapis.com/world/2020/jan/06/iranian-ambassador-hits-out-provocative-times-article-uk',
+    isHosted: false,
+    pillarId: 'pillar/news',
+    pillarName: 'News',
+  },
+]
+
+// const getGoods = () => {
+//   const goods =
+// fetch(`https://content.guardianapis.com/tags?page=1&page-size=9&q=blog&api-key=b41e2e26-a0d2-488d-8155-855ade5f1227`)
+//     .then(response => response.json());
+
+//   return goods;
+// };
+
+const showGoods = () => {
+  // const result = await getGoods();
+  // const news = result.response.results;
+
+  const newsList = document.querySelector('#news');
+  const pagination = document.querySelector('#pagination');
+  const notesOnPage = 9;
+  const countOfItems = Math.ceil(news.length / notesOnPage);
+
+  const showPage = (function() {
+    let active;
+
+    return function(item) {
+      if (active) {
+        active.classList.remove('pagination__item--active');
+      }
+
+      active = item;
+
+      item.classList.add('pagination__item--active');
+
+      const pageNum = +item.innerHTML;
+      const start = (pageNum - 1) * notesOnPage;
+      const end = start + notesOnPage;
+      const articles = news.slice(start, end);
+
+      newsList.innerHTML = '';
+
+      for (const article of articles) {
+        const newsItem = document.createElement('li');
+
+        newsItem.classList.add('news__item');
+        newsList.appendChild(newsItem);
+
+        const newsTop = document.createElement('div');
+
+        newsTop.classList.add('news__top');
+        newsItem.appendChild(newsTop);
+
+        const newsBottom = document.createElement('div');
+
+        newsBottom.classList.add('news__bottom');
+        newsItem.appendChild(newsBottom);
+
+        createBox(article.sectionName, newsBottom);
+        createBox(article.webTitle, newsBottom);
+        createBox(article.webUrl, newsBottom);
+      }
+    };
+  }());
+
+  const paginationItems = [];
+
+  for (let i = 1; i <= countOfItems; i++) {
+    const li = document.createElement('li');
+
+    li.classList.add('pagination__item');
+    li.innerHTML = i;
+    pagination.appendChild(li);
+    paginationItems.push(li);
+  }
+
+  showPage(paginationItems[0]);
+
+  for (const paginationItem of paginationItems) {
+    paginationItem.addEventListener('click', function() {
+      showPage(this);
+    });
+  }
+
+  function createBox(text, li) {
+    const div = document.createElement('div');
+
+    div.innerHTML = text;
+    li.appendChild(div);
+  }
+};
+
+showGoods();
