9ae15ba633e46835069b154caeaf52886176653f
Vitalii <vitaliybondarenko1982@gmail.com>
2020-03-30 23:02:47 +0300

    wrapped all item in link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
-/* eslint-disable max-len */
-const NEWS_URL = `http://content.guardianapis.com/search?from-date=2020-03-27&to-date=2020-03-28&order-by=newest&show-fields=all&page-size=90&api-key=b41e2e26-a0d2-488d-8155-855ade5f1227`;
+// eslint-disable-next-line max-len
+const NEWS_URL = `http://content.guardianapis.com/search?from-date=2020-03-28&to-date=2020-03-28&order-by=newest&show-fields=all&page-size=90&api-key=b41e2e26-a0d2-488d-8155-855ade5f1227`;
 
-const getNews = () => {
-  const news = fetch(NEWS_URL)
-    .then(response => response.json());
+const getNews = async(url) => {
+  const response = await fetch(url);
+  const data = await response.json();
 
-  return news;
+  return data;
 };
 
 const newsList = document.querySelector('#news');
 const pagination = document.querySelector('#pagination');
 
 const showNews = async() => {
-  const result = await getNews();
+  const result = await getNews(NEWS_URL);
   const news = result.response.results;
 
   const notesOnPage = 9;
@@ -44,25 +44,25 @@ const showNews = async() => {
         const fullNewsLink = document.createElement('a');
         const image = document.createElement('img');
         const sectionName = document.createElement('span');
-        const title = document.createElement('a');
+        const title = document.createElement('p');
         const text = document.createElement('p');
         const author = document.createElement('p');
 
         createBox(newsItem, 'news__item', newsList);
-        createBox(fullNewsLink, 'news__link-article', newsItem);
+        createBox(fullNewsLink, 'news__link', newsItem);
         createBox(image, 'news__image', fullNewsLink);
-        createBox(sectionName, 'news__section-name', newsItem);
-        createBox(title, 'news__link', newsItem);
-        createBox(text, 'news__text', newsItem);
-        createBox(author, 'news__author', newsItem);
+        createBox(sectionName, 'news__section-name', fullNewsLink);
+        createBox(title, 'news__title', fullNewsLink);
+        createBox(text, 'news__text', fullNewsLink);
+        createBox(author, 'news__author', fullNewsLink);
 
         sectionName.innerHTML = article.sectionName;
         text.innerHTML = article.fields.trailText;
         title.innerHTML = article.fields.headline;
         author.innerHTML = article.fields.byline;
-        fullNewsLink.setAttribute('href', `article.html/id=${article.id}`);
+
+        fullNewsLink.setAttribute('href', article.webUrl);
         image.setAttribute('alt', article.apiUrl);
-        title.setAttribute('href', article.webUrl);
         image.setAttribute('src', article.fields.thumbnail);
       }
     };
@@ -91,12 +91,12 @@ const showNews = async() => {
     });
   }
 
-  newsList.addEventListener('click', async function(evt) {
+  newsList.addEventListener('click', function(evt) {
     evt.preventDefault();
 
-    const { alt } = evt.target;
-
-    const article = news.find(item => item.apiUrl === alt);
+    const targetItem = evt.target.closest('.news__link');
+    const href = targetItem.getAttribute('href');
+    const article = news.find(item => item.webUrl === href);
 
     location.assign(`${window.location}article.html?id=${article.id}`);
   });
